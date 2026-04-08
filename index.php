ccbae784b38eefc0e56e1b5e0b9513e8373e7967
Yuriy Bevov <yuriybevov@gmail.com>
2026-04-08 15:43:21 +0300

    Добавил кнопку со стрелкой

--- index.php
+++ index.php
@@ -3,7 +3,19 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Мебельная компания");
 ?>
 
-<div class="" style="margin-top:300px;">1</div>
+<section class="section hero">
+  <div class="container">
+    <button class="arrow-btn">
+      <span class="arrow-btn__text">Оставить заявку</span>
+      <span class="arrow-btn__icon">
+        <svg width='24' height='24' role='img' aria-hidden='true' focusable='false'>
+          <use xlink:href='<?= SITE_TEMPLATE_PATH ?>/_dist/sprite.svg#arrow'></use>
+        </svg>
+      </span>
+    </button>
+  </div>
+</section>
+
 <div class="" style="margin-top:300px;">1</div>
 <div class="" style="margin-top:300px;">1</div>
 <div class="" style="margin-top:300px;" data-speed="3">3</div>
